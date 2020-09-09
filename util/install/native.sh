60f378e502c2e90de03ce3cb65dfcc530481f792
RepoCloudSwyft <35359894+RepoCloudSwyft@users.noreply.github.com>
2020-09-10 04:41:59 +0800

    Update native.sh

--- util/install/native.sh
+++ util/install/native.sh
@@ -99,10 +99,17 @@ sudo apt-get upgrade -y
 ##
 ## Install system pre-requisites
 ##
-sudo apt-get install -y build-essential software-properties-common curl git-core libxml2-dev libxslt1-dev python-pip libmysqlclient-dev python-apt python-dev libxmlsec1-dev libfreetype6-dev swig gcc g++
-sudo pip install --upgrade pip==9.0.3
-sudo pip install --upgrade setuptools==39.0.1
-sudo -H pip install --upgrade virtualenv==15.2.0
+#sudo apt-get install -y build-essential software-properties-common curl git-core libxml2-dev libxslt1-dev python-pip libmysqlclient-dev python-apt python-dev libxmlsec1-dev libfreetype6-dev swig gcc g++
+#sudo pip install --upgrade pip==9.0.3
+#sudo pip install --upgrade setuptools==39.0.1
+#sudo -H pip install --upgrade virtualenv==15.2.0
+#sudo apt-get install -y build-essential software-properties-common curl git-core libxml2-dev libxslt1-dev python-pip libmysqlclient-dev python-apt python-dev libxmlsec1-dev libfreetype6-dev swig gcc g++
+
+# ansible-bootstrap installs yaml that pip 19 can't uninstall.
+sudo apt-get remove -y python-yaml
+sudo pip install --upgrade pip==20.0.2
+sudo pip install --upgrade setuptools==44.1.0
+sudo -H pip install --upgrade virtualenv==16.7.10
 
 ##
 ## Overridable version variables in the playbooks. Each can be overridden
@@ -147,7 +154,7 @@ CONFIGURATION_VERSION=${CONFIGURATION_VERSION-$OPENEDX_RELEASE}
 ## Clone the configuration repository and run Ansible
 ##
 cd /var/tmp
-git clone https://github.com/edx/configuration
+git clone https://github.com/CS-Ironwood/configuration
 cd configuration
 git checkout $CONFIGURATION_VERSION
 git pull
